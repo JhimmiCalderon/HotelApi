156b89a6a40c2806ba15a8e5c852cf7eafad9cf7
unknown <alexandercalleagudelo@gmail.com>
2023-09-18 07:25:00 -0500

    PA 18/09/2023 Se realiza las cinco solicitudes de Reservas

--- index.js
+++ index.js
@@ -19,6 +19,24 @@ app.delete('/api/habitaciones', function (req, res) {
   res.send('Estoy eliminando una Habitación')
 })
 
+//Atendiento Reservas
+
+app.post('/api/reservas', function (req, res) {
+  res.send('Estoy Guardando una Reserva')
+})
+app.get('/api/reservas', function (req, res) {
+  res.send('Estoy Buscando Todas Las Reservas')
+})
+app.get('/api/reserva', function (req, res) {
+  res.send('Estoy Buscando una Reserva')
+})
+app.put('/api/reservas', function (req, res) {
+  res.send('Estoy Actualizando una Reserva')
+})
+app.delete('/api/reservas', function (req, res) {
+  res.send('Estoy eliminando una Reserva')
+})
+
 //Levantando un servidor
 app.listen(3000,function(){
     console.log("servidor operando")
